b4e8b57696a8f4f814df2d7dfc57e39bba5c29fd
zenon-paul <123747386+zenon-paul@users.noreply.github.com>
2023-03-16 22:04:45 +0900

    Update rubiks_cube_graphic.h

--- rubiks_cube_graphic.h
+++ rubiks_cube_graphic.h
@@ -13,7 +13,7 @@
 #define Y_AXIS 30
 #define Z_AXIS 30
 #define PI 3.1415
-#define RA 90
+#define RA 180
 #define EG 20
 #define LE 0
 #define MI 20
@@ -26,22 +26,21 @@ typedef struct data_face {
 }d_face;
 
 typedef void(*rfunc)(double, double*);
-extern double cube[6][4][3];
-extern int face_set[6][8];
+//extern double cube[6][4][3];
+//extern int face_set[6][8];
 extern int face_posi[6][8];
-extern double corner_seed[8][3];
-extern double edge_seed[12][3];
+//extern double corner_seed[8][3];
+//extern double edge_seed[12][3];
 extern d_face corners[8][6];
 extern d_face edges[12][6];
 extern d_face axises[6][6];
 void init_corner();
 void init_edge();
 void init_axises();
-void init_cube_color();
-void init_face_posi(int posi[6][8]);
+void init_cube_color(double colors[6][3]);
+//void init_face_position(int posi[6][8]);
 void product(double matrix[3][3], double vector[3]);
-void simuration(d_state* state, int i);
-void update_face_posi(d_state state, int face[6][8]);
 void reset();
 void graphic(int argc, char** argv);
+void key_setting(int i, char x);
 #endif
