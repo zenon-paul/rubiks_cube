6ed0e988a18c4d88b3b7b66fd1e413110d308f9e
zenon-paul <123747386+zenon-paul@users.noreply.github.com>
2023-03-16 22:04:13 +0900

    Update rubiks_cube_definition.h

--- rubiks_cube_definition.h
+++ rubiks_cube_definition.h
@@ -8,6 +8,17 @@
 #define PHASE1_MOVE 18
 #define PHASE2_MOVE 10
 #define VECTOR_LEN 60
+#define NUM_CORNER 8
+#define NUM_EDGE 12
+#define R_CENTER 4
+#define L_CENTER 13
+#define F_CENTER 22
+#define B_CENTER 31
+#define U_CENTER 40
+#define D_CENTER 49
+#define NUM_FACE 6
+#define NUM_3_SQUARED 9
+#define GENERATION 100
 
 typedef struct s_data {
 	int cp[8];
@@ -17,23 +28,19 @@ typedef struct s_data {
 }d_state;
 
 typedef void(*func)(d_state*);
-extern int path1[VECTOR_LEN];
-extern int path2[VECTOR_LEN];
-extern int mnum1;
-extern int mnum2;
-extern func phase1_move[PHASE1_MOVE];
-extern func phase2_move[PHASE2_MOVE];
+extern int path1[VECTOR_LEN][GENERATION];
+extern int path2[VECTOR_LEN][GENERATION];
+extern int mnum1[GENERATION];
+extern int mnum2[GENERATION];
+extern int motion_count;
+extern int motion_list[VECTOR_LEN];
+//extern func phase1_move[PHASE1_MOVE];
+//extern func phase2_move[PHASE2_MOVE];
 extern int coorresponding_table_from_phase2_move_to_phase1_move[PHASE2_MOVE];
-extern d_state state;
+//extern d_state state;
 void disp_state(d_state x);
 void init_state(d_state* x);
 void clear_state(d_state* x);
-void make_r(d_state* x);
-void make_b(d_state* x);
-void make_l(d_state* x);
-void make_f(d_state* x);
-void make_u(d_state* x);
-void make_d(d_state* x);
 void init_move();
 void R(d_state* x);
 void B(d_state* x);
@@ -53,5 +60,10 @@ void L_(d_state* x);
 void F_(d_state* x);
 void U_(d_state* x);
 void D_(d_state* x);
-void start_search(d_state start0);
+void search(d_state start);
+d_state* get_state_value_address();
+void phase1_motion(d_state* x, int i);
+void phase2_motion(d_state* x, int i);
+void init_motion_list();
+int read_csv_file();
 #endif
